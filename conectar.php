def239e8954d72190007ee4a77f3b0460769a295
Ademar Perfoll Junior <junior.bsn@gmail.com>
2018-11-28 16:22:13 -0200

    Atualização do arquivo de conexão para dar suporte aos acentos e carateres especiais!

--- conectar.php
+++ conectar.php
@@ -5,6 +5,7 @@
   try {
     $conn = new PDO("mysql:host=$servidor;dbname=empresa",$username,$password);
     $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8");
     //echo "Conectou com sucesso!";
   } catch(PDOException $erro) {
       echo "Falha de conexão: ".$erro->getMessage();
